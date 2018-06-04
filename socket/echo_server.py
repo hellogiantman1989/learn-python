419969bf071c77f9277718201e17c67a904833ad
hellogiantman1989 <33917937+hellogiantman1989@users.noreply.github.com>
2018-06-04 13:46:46 +0800

    Update echo_server.py
    str encode byte

--- socket/echo_server.py
+++ socket/echo_server.py
@@ -6,22 +6,22 @@
 import time, socket, threading
 
 def tcplink(sock, addr):
-    print 'Accept new connection from %s:%s...' % addr
-    sock.send('Welcome!')
+    print ('Accept new connection from %s:%s...' % addr)
+    sock.send('Welcome!'.encode())
     while True:
         data = sock.recv(1024)
         time.sleep(1)
-        if data == 'exit' or not data:
+        if data == 'exit'.encode() or not data:
             break
-        sock.send('Hello, %s!' % data)
+        sock.send('Hello, %s!'.encode() % data)
     sock.close()
-    print 'Connection from %s:%s closed.' % addr
+    print ('Connection from %s:%s closed.' % addr)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 监听端口:
 s.bind(('127.0.0.1', 9999))
 s.listen(5)
-print 'Waiting for connection...'
+print ('Waiting for connection...')
 while True:
     # 接受一个新连接:
     sock, addr = s.accept()
