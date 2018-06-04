c103fd2e373e08ff9f0939097ffcaa75c45ac005
hellogiantman1989 <33917937+hellogiantman1989@users.noreply.github.com>
2018-06-04 13:45:30 +0800

    Update echo_client.py

--- socket/echo_client.py
+++ socket/echo_client.py
@@ -9,10 +9,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
 s.connect(('127.0.0.1', 9999))
 # 接收欢迎消息:
-print s.recv(1024)
-for data in ['Michael', 'Tracy', 'Sarah']:
+print (s.recv(1024))
+for data in ['Michael', 'Tracy', 'Sarah', '102,204,321,540']:
     # 发送数据:
-    s.send(data)
-    print s.recv(1024)
-s.send('exit')
+    s.send(data.encode())
+    print (s.recv(1024))
+s.send('exit'.encode())
 s.close()
